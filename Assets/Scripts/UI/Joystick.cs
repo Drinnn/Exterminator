515c571022da552a7d803fb59cde390bf1c20f40
Drinnn <pedro_lontro@hotmail.com>
2023-06-29 21:58:34 -0300

    chore: prepare joystick

---
+++ Assets/Scripts/UI/Joystick.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UI
+{
+    public class Joystick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+    {
+        public void OnDrag(PointerEventData eventData)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }    
+}
